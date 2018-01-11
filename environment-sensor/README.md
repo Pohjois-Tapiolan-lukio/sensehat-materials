8bdec71692ef3c301421526c60a8f5faf47fb794
excitedneon <excitedneon@gmail.com>
2018-01-11 13:32:12 +0200

    Implement env sensor

---
+++ environment-sensor/README.md
@@ -0,0 +1,2 @@
+# Environment sensor
+Polls the environment sensors and displays a graph. Temperature in red, pressure in gray, humidity in blue.
---
+++ environment-sensor/environment-sensor.py
@@ -0,0 +1,28 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+def map_to_leds(value, start, end):
+    return int(max(0, min(1, (value - start) / (end - start))) * 6)
+
+# stat_position is 0-2, decides the horizontal position of the visualization (left-to-right)
+# range_start and range_end define the stat's desired min and max values
+# r, g and b decide the stat visualization's color
+def display_stat(stat_position, value, range_start, range_end, r, g, b):
+    global sense
+    value = map_to_leds(value, range_start, range_end)
+    for y in range(7 - value, 7):
+        sense.set_pixel(1 + stat_position * 2, y, r, g, b)
+        sense.set_pixel(2 + stat_position * 2, y, r, g, b)
+
+while True:
+    temp = sense.get_temperature()
+    pressure = sense.get_pressure()
+    humidity = sense.get_humidity()
+    print("{:.1f} C, {:.1f} hPa, {:.1f} %".format(temp, pressure, humidity))
+    sense.clear()
+    display_stat(0, temp, 22, 28, 255, 128, 0)
+    display_stat(1, pressure, 1011, 1015, 160, 160, 160)
+    display_stat(2, humidity, 25, 75, 0, 128, 255)
+    sleep(0.5)
